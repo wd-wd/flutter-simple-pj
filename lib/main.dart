020c457161bc5acd37c91281f61490106535de7b
wangdong <690931210@qq.com>
2020-12-04 16:16:57 +0800

    侧边栏ui布局实现

--- lib/main.dart
+++ lib/main.dart
@@ -29,17 +29,14 @@ class CommonTitle extends StatelessWidget {
       ),
       //注意脚手架带的组件，不在appbar中可以看源码tips:在appBar中加了actions
       // 后endDrawer不显示,加上leading后drawer不显示
-      drawer: Drawer(//左边栏
-
-      ),
-      endDrawer: Drawer(//右边栏
-
-      ),
+      drawer: MyDrawer(), //左边栏
+      endDrawer: MyDrawer(), //右边栏
       body: MyContent(),
     );
   }
 }
 
+//实现内容
 class MyContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -48,6 +45,46 @@ class MyContent extends StatelessWidget {
         '主页',
         style: TextStyle(
             color: Colors.blue, fontSize: 28.0, fontWeight: FontWeight.w700),
+      ),
+    );
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.all(0),
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountEmail: Text('690931210@qq.com'),
+            accountName: Text('大东'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83epcZcn3N0bED2vlv02MT4L4mVYdKTdRwgibxaRrIHMVHbe0Uv5X3GclpR7IPtUrQREU6lNp8JL7JUA/132'),
+            ),
+            //美化组件的装饰，很多组件都有这个属性
+            decoration: BoxDecoration(color: Color(int.parse('0xFFF44336'))),
+          ),
+          ListTile(
+            title: Text('个人简介'),
+            trailing: Icon(Icons.verified_user),
+          ),
+          ListTile(
+            title: Text('在职公司'),
+            trailing: Icon(Icons.settings_input_component),
+          ),
+          ListTile(
+            title: Text('行业背景'),
+            trailing: Icon(Icons.style),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('设置'),
+            trailing: Icon(Icons.settings),
+          ),
+        ],
       ),
     );
   }
