80ef2af78cee5bae9b7dd6f8763ec10c4e63b7cd
wangdong <690931210@qq.com>
2020-12-05 14:49:16 +0800

    添加dio

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-
+var dio = new Dio();
 //因为home是要结合接口数据实现的 所以使用有状态的组件
 class HomePage extends StatefulWidget {
   //写构造函数，在dart语言中的构造函数写法
@@ -16,10 +17,19 @@ class HomePage extends StatefulWidget {
 //一半写法是下划线加要管理的类名加state
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    getMovieList();
+  }
+  @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
       child: Text('首页'+widget.type),
     );
+  }
+  getMovieList() async {
+   var homeResponse = await dio.get('http://test.api.happygo.com/product/query/spuPage?query.sourceType=7&query.id=422&page=0&size=10&query.isSort=false');
+    print(homeResponse);
   }
 }
